1acb11047634b229d6f9bb63ca2dbf08aaa7bd68
Aditya Gaur <30363732+aditya-gaur@users.noreply.github.com>
2018-06-09 21:31:22 +0530

    changed the API URL to secure http

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API_KEY = "d94bcd435b62a031771c35633f9f310a";
-const baseUrl = "http://api.openweathermap.org/data/2.5/";
+const baseUrl = "https://api.openweathermap.org/data/2.5/";
 
 const fetchWeather = city => {
   const apiUrl = `${baseUrl}/forecast/daily?q=${city}&units=metric&cnt=7&appid=${API_KEY}`;
